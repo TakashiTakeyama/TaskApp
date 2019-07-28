5262bd3327c919d339cb363f595592cc51e90f37
Takashi <Takashi@gmail.com>
2019-07-28 14:34:40 +0900

    Basic認証を実装した2回目

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -48,6 +48,10 @@ class BlogsController < ApplicationController
     params.require(:blog).permit(:name, :details)
   end
 
+  def production?
+    Rails.env.production?
+  end
+
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]

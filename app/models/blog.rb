53d8c76f68cf4733325f930b40cb670b39ecb228
Takashi <Takashi@gmail.com>
2019-07-31 19:32:04 +0900

    優先順位を設定しました

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,5 @@
 class Blog < ApplicationRecord
   validates :name, presence: true
   validates :details, presence: true
+  enum priority:{"低": 0, "中": 1, "高": 2}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_053323) do
+ActiveRecord::Schema.define(version: 2019_07_31_080825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,6 @@ ActiveRecord::Schema.define(version: 2019_07_31_053323) do
     t.datetime "updated_at", null: false
     t.datetime "expired_at", default: "2019-07-28 19:00:50", null: false
     t.string "state", default: "完了", null: false
+    t.integer "priority", default: 0, null: false
   end
-
 end

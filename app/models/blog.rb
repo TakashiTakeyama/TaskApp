6c50ffac2dea49c268d8b90572bbb47608b6cd08
Takashi <Takashi@gmail.com>
2019-08-02 15:43:56 +0900

    Userモデルを作成しました

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,5 @@ class Blog < ApplicationRecord
   validates :name, presence: true
   validates :details, presence: true
   enum priority:{"低": 0, "中": 1, "高": 2}
+  belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,31 @@
+# 10.times do |n|
+#   name = "仕事"
+#   details = "打ち合わせ"
+#   Blog.create!(name: name,
+#                details: details,
+#                )
+# end
+#
+# 10.times do |n|
+#   name = "勉強"
+#   details = "rails"
+#   Blog.create!(name: name,
+#                details: details,
+#                )
+# end
+#
+# 10.times do |n|
+#   name = "トレーニング"
+#   details = "背中"
+#   Blog.create!(name: name,
+#                details: details,
+#                )
+# end
+#
 10.times do |n|
-  name = "仕事"
-  details = "打ち合わせ"
-  Blog.create!(name: name,
-               details: details,
-               )
-end
-
-10.times do |n|
-  name = "勉強"
-  details = "rails"
-  Blog.create!(name: name,
-               details: details,
-               )
-end
-
-10.times do |n|
-  name = "トレーニング"
-  details = "背中"
-  Blog.create!(name: name,
-               details: details,
+  name = Faker::Name.name
+  email = Faker::Internet.email
+  User.create!(name: name,
+               email: email,
                )
 end
